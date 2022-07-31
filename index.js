1c95bd2477877661095c198330ec11b49f68712d
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-07-30 20:34:16 -0400

    got the view dept, view role, and view empl functions to work as intended

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 const cTable = require("console.table");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const figlet = require("figlet");
-
-const log = console.log;
 
 // Connect to database
 const db = mysql.createConnection(
@@ -16,30 +13,22 @@ const db = mysql.createConnection(
 		password: "bulldog",
 		database: "company_db",
 	},
-	log(`Connected to the company_db database.`)
+	// retun that the connection has been established, and psot the banner
+	console.log(`Connected to the company_db database \n\n    --------------------------
+    EMPLOYEE MANAGEMENT SYSTEM
+    -------------------------- \n`)
 );
 
 // for SELECT IFNULL(X,Y): if x ISN'T null, return x; if x IS null, return y
 
-// on start, give menu
-// view all depts (show ids and names)
-// view all roles (show id , title, dept, salary)
-// view all employees (show id, first, last, role, dept, salary, manager)
-// add a department (prompt for name)
-// add a role (prompt for name, salary, dept)
-// add an employee (prompt for first, last, role, manager)
-// update employee role (prompt for employee, enter new role)
-
-// initial function to call the banner and get the ball rolling
-function init() {
-	figlet("EMPLOYEE MANAGEMENT SYSTEM", (err, text) => {
-		if (err) {
-			log(err);
-		}
-		log(text);
-	});
-	menu();
-}
+// TODOx: on start, give menu
+// TODOx: view all depts (show ids and names)
+// TODOx: view all roles (show id , title, dept, salary)
+// TODOx: view all employees (show id, first, last, role, dept, salary, manager)
+// TODO: add a department (prompt for name)
+// TODO: add a role (prompt for name, salary, dept)
+// TODO: add an employee (prompt for first, last, role, manager)
+// TODO: update employee role (prompt for employee, enter new role)
 
 // menu function to ask what the user would like to do
 function menu() {
@@ -91,8 +80,8 @@ function menu() {
 
 // function to view all departments
 function viewAllDept() {
-	log("\n");
-    db.query(
+	console.log("\n");
+	db.query(
 		`SELECT department.id, department.name AS "Department" from department`,
 		function showTable(err, output) {
 			console.table(output);
@@ -101,7 +90,29 @@ function viewAllDept() {
 	);
 }
 
+// function to view all roles
+function viewAllRole() {
+	console.log("\n");
+	db.query(
+		`SELECT role.title AS Job_Title, role.id AS ID, department.name AS Department, role.salary AS Salary FROM role JOIN department ON role.department_id = department.id ORDER BY role.id`,
+		function showTable(err, output) {
+			console.table(output);
+			menu();
+		}
+	);
+}
 
+// function to view all employees
+function viewAllEmpl() {
+	console.log("\n");
+	db.query(
+		`SELECT employee.id AS Employee_ID, CONCAT (employee.first_name," ", employee.last_name) AS Name, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(manager.first_name, " ", manager.last_name) AS Manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id ORDER BY employee.id`,
+		function showTable(err, output) {
+			console.table(output);
+			menu();
+		}
+	);
+}
 
 // call for the app to run
-init();
+menu();
